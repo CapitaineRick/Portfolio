5bdfb15f58992b136efc4b317c06d65578b71368
Rick <124872857+CapitaineRick@users.noreply.github.com>
2025-05-25 01:15:37 +0200

    Updated PdfViewer.jsx

--- src/components/PdfViewer.jsx
+++ src/components/PdfViewer.jsx
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import { Document, Page } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
+import { pdfjs } from 'react-pdf';
+import workerSrc from 'pdfjs-dist/build/pdf.worker.entry';
+
+pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
+
 
 export default function PdfViewer({ file }) {
   const [numPages, setNumPages] = useState(null);
