1f83011c2300552a657f2ecb4cce0cda1ba678eb
Rick <124872857+CapitaineRick@users.noreply.github.com>
2025-05-24 19:13:40 +0200

    Streamline site structure and reduce subdivisions

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,8 @@ import { ProjectProvider } from './contexts/ProjectContext';
 import Layout from './components/Layout';
 import Hero from './components/sections/Hero';
 import About from './components/sections/About';
-import Education from './components/sections/Education';
 import Skills from './components/sections/Skills';
 import Projects from './components/sections/Projects';
-import Documentation from './components/sections/Documentation';
 import TechWatch from './components/sections/TechWatch';
 import Contact from './components/sections/Contact';
 
@@ -18,10 +16,8 @@ function App() {
         <Layout>
           <Hero />
           <About />
-          <Education />
           <Skills />
           <Projects />
-          <Documentation />
           <TechWatch />
           <Contact />
         </Layout>
