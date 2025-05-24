3863cc0c4d3abf8dcf75cbd4903c16d9e7a00f87
Rick <124872857+CapitaineRick@users.noreply.github.com>
2025-05-24 23:33:52 +0200

    Fix PDF.js worker file path

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,8 @@ import App from "./App";
 import "./index.css";
 import { pdfjs } from 'react-pdf';
 
-// Configure PDF.js worker to use local file - removed leading slash for correct path resolution
-pdfjs.GlobalWorkerOptions.workerSrc = 'pdfjs/web/pdf.worker.min.js';
+// Update worker path to correct location relative to public directory
+pdfjs.GlobalWorkerOptions.workerSrc = '/pdfjs/pdf.worker.min.js';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
