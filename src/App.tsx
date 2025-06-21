99f4b6dd5218b4ee3d4c57c8b0dd2bcf26c67f95
CapitaineRick <sebastien.78.fernandes@outlook.fr>
2025-06-21 14:46:45 +0200

    data

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import TechWatch from './components/sections/TechWatch';
 import Contact from './components/sections/Contact';
 import Education from './components/sections/Education';
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
