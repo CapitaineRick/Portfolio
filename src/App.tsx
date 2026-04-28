4bfa3240719e890794e405d17f35a926bebef43e
sebastien fernandes <sebastien.78.fernandes@outlook.fr>
2026-04-28 11:18:14 +0100

    maj

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { ProjectProvider } from './contexts/ProjectContext';
 import Layout from './components/Layout';
 import Hero from './components/sections/Hero';
 import About from './components/sections/About';
-import Skills from './components/sections/Skills';
+
 import Projects from './components/sections/Projects';
 import TechWatch from './components/sections/TechWatch';
 import Contact from './components/sections/Contact';
