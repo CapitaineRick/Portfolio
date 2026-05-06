66dc74502d58d0b05b20c222acab04b9528a2f37
CapitaineRick <sebastien.78.fernandes@outlook.fr>
2026-05-06 17:32:58 +0200

    maj

--- src/data/techWatchData.ts
+++ src/data/techWatchData.ts
@@ -70,7 +70,7 @@ SecNumCloud n'est pas un label de souveraineté technologique. La qualification 
 
 SecNumCloud s'impose comme la norme de référence pour la protection des données sensibles en France, rendant sa maîtrise indispensable pour tout professionnel du réseau et de la cybersécurité.`,
 
-  image: "public\images\secnumcloud.png",
+  image: "\images\secnumcloud.png",
   date: "01 mai 2026",
   tags: ["SecNumCloud", "ANSSI", "Cloud", "Cybersécurité", "Souveraineté numérique", "Cloud de confiance"],
   category: "Cybersécurité",
